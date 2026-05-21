67196e1a678ab02f567f577a35a3dc7855b40902
jeronimomartinez942 <jeronimo.martinez942@pascualbravo.edu.co>
2026-05-21 12:24:02 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,34 @@
-﻿internal class Program
+using System;
+
+internal class Program
 {
-    public static void Main(string[]args)
+    public static void Main(string[] args)
     {
-        punto4.menu();
+        int opcion;
+
+        do
+        {
+            Console.WriteLine("\nMENU PRINCIPAL");
+            Console.WriteLine("1. Punto 1");
+            Console.WriteLine("2. Punto 3");
+            Console.WriteLine("3. Punto 4");
+            Console.WriteLine("4. Salir");
+
+            opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    punto1.ips();
+                    break;
+                case 2:
+                    punto3.registro_estudiantes();
+                    break;
+                case 3:
+                    punto4.menu();
+                    break;
+            }
+
+        } while (opcion != 4);
     }
 }
