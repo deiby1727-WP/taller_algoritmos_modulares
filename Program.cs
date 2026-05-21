59e980c4673667c035f2806f0f24ca48c5030253
jeronimomartinez942 <jeronimo.martinez942@pascualbravo.edu.co>
2026-05-21 12:34:44 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,8 +9,8 @@ internal class Program
         do
         {
             Console.WriteLine("\nMENU PRINCIPAL");
-            Console.WriteLine("1. Punto 1");
-            Console.WriteLine("2. Punto 3");
+            Console.WriteLine("1. Punto 1 (IPS)");
+            Console.WriteLine("2. Punto 3 (Estudiantes)");
             Console.WriteLine("3. Punto 4");
             Console.WriteLine("4. Salir");
 
@@ -19,11 +19,13 @@ internal class Program
             switch (opcion)
             {
                 case 1:
-                    punto1.ips();
+                    punto1.menu();
                     break;
+
                 case 2:
-                    punto3.registro_estudiantes();
+                    punto3.menu();
                     break;
+
                 case 3:
                     punto4.menu();
                     break;
